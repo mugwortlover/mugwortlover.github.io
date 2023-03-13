0f3373de40141f7c9711d87f1dd53c9f52b4e824
mugwortlover <97547989+mugwortlover@users.noreply.github.com>
2023-03-13 16:58:51 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+isDark = false
 function switchMode() {
-    document.body.classList.toggle('dark-mode')
+    const page = document.getElementById('page')
+    if (isDark == true) {
+        page.style.color = 'black'
+        page.style.backgroundColor = 'white'
+        isDark = false
+    } else {
+        page.style.color = 'white'
+        page.style.backgroundColor = 'black'
+        isDark = true
+    }
+    console.log(isDark)
+}
+
+function draw() {
+	const plot = document.getElementById("canvas") ;
+	const ctx = plot.getContext("2d");	
+    ctx.fillStyle = 'green';
+	ctx.fillRect(50, 50, 50, 50);
+
+    console.log('called')
 }
